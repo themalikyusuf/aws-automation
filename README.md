3a1ac36af9ac73eb2792e88d122fee7acf27d3ae
Abdulmalik Yusuf <themalikyusuf@gmail.com>
2020-03-21 11:49:43 +0100

    feat: add backup script and readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # mongodb-s3-backup
+
+##### Requirements
+  - AWS access and secret keys for the 'mongodb' user. This can be created in the Identity and Access Management(IAM) page on AWS's console. The 'mongodb' user has been given full programmatic access to S3(Where the backup will be store)
+ 
+##### Note
+- The required tools for this script(s3cmd and wget) have already been installed on the MongoDB server as part of the CloudFormation script.
+- Before running this script, enter the command below to configure `s3cmd` and follow the prompt to input the AWS access/secret keys. Enter **us-east-2** as the region.
+
+```
+$ s3cmd --configure
+```
+##### Running The Script:
+
+Before running the script, ensure to have the appropriate variables in the file i.e.
+- COLLECTIONS
+- S3_BUCKET_NAME
+- MONGO_DATABASE
+```
+$ ./mongodb.sh
+```
+Also, ensure to set a cron job as described at the end of script.
--- script.sh
+++ script.sh
@@ -13,3 +13,42 @@
 # Run the command below to configure s3cmd. Enter aws access/secret key: create access/secret key under the mongodb user in IAM. Region: us-east-2
 ##########
 # s3cmd --configure
+
+# Set vars
+mongo admin --eval "printjson(db.fsyncLock())"
+MONGODUMP_PATH="/usr/bin/mongodump"
+TIMESTAMP=`date +%F-%H%M`
+S3_BUCKET_PATH="mongodb-backups"
+
+MONGO_DATABASE="DATABASE NAME" # Replace with database name
+S3_BUCKET_NAME="S3 BUCKET NAME" # Replace with bucket name on S3: terminus-mongodb-production
+COLLECTIONS=("foo" "bar") # Add collections from terminus_properties database in the array
+
+for i in "${COLLECTIONS[@]}"
+do
+
+	# Create dump
+	$MONGODUMP_PATH -d $MONGO_DATABASE -c $i
+
+	# Add timestamp and tar dump
+	mv dump mongodb-$i-$TIMESTAMP
+	tar cf mongodb-$i-$TIMESTAMP.tar mongodb-$i-$TIMESTAMP
+
+	# Upload to S3
+	s3cmd put mongodb-$i-$TIMESTAMP.tar s3://$S3_BUCKET_NAME/$S3_BUCKET_PATH/mongodb-$i-$TIMESTAMP.tar
+
+	#Unlock database writes
+	mongo admin --eval "printjson(db.fsyncUnlock())"
+
+	#Delete local files
+	rm -rf mongodb-*
+
+done
+
+#########
+# Run the cron job command to set up back for 02:00 daily(Note timezones)
+#########
+# 00 02 * * * /mongodb.sh
+
+
+# Reference: https://medium.com/@bansalnagesh/backing-up-mongodb-on-aws-ec2-to-s3-b045b5727fd6
