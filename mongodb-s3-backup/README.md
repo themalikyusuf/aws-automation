0aae898d3be2baf7f8f55a9baa9d8ec080f721f4
Abdulmalik Yusuf <themalikyusuf@gmail.com>
2020-04-03 10:21:37 +0100

    chore: move mongo script and cleanup

---
+++ mongodb-s3-backup/README.md
@@ -0,0 +1,20 @@
+## mongodb-s3-backup
+A Shell script that backs up MongoDB data on EC2 to S3.
+
+#### Requirements
+  - AWS access and secret keys with full programmatic access to S3. Get them from [IAM](https://console.aws.amazon.com/iam).
+
+#### Prerequisites
+Replace the following dummy variables in the `script.sh` file:
+- COLLECTIONS
+- S3_BUCKET_NAME
+- MONGO_DATABASE
+
+#### How to use
+From the root directory:
+```
+$ ./script.sh
+```
+
+##### Credit
+[Nagesh Bansal](https://medium.com/@bansalnagesh/backing-up-mongodb-on-aws-ec2-to-s3-b045b5727fd6)
---
+++ mongodb-s3-backup/script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+apt-get update
+apt-get install wget -y
+wget -O- -q http://s3tools.org/repo/deb-all/stable/s3tools.key | apt-key add -
+wget -O/etc/apt/sources.list.d/s3tools.list http://s3tools.org/repo/deb-all/stable/s3tools.list
+apt-get install s3cmd -y
+
+# Todo: set up s3cmd
+
+# Set vars
+mongo admin --eval "printjson(db.fsyncLock())"
+MONGODUMP_PATH="/usr/bin/mongodump"
+TIMESTAMP=`date +%F-%H%M`
+S3_BUCKET_PATH="mongodb-backups"
+
+MONGO_DATABASE="DATABASE NAME" # Replace with database name
+S3_BUCKET_NAME="S3 BUCKET NAME" # Replace with S3 bucket name
+COLLECTIONS=("foo" "bar") # Add collections from database in the array
+
+for i in "${COLLECTIONS[@]}"
+do
+
+	# Create dump
+	$MONGODUMP_PATH -d $MONGO_DATABASE -c $i
+
+	# Add timestamp and tar dump
+	mv dump mongodb-$i-$TIMESTAMP
+	tar cf mongodb-$i-$TIMESTAMP.tar mongodb-$i-$TIMESTAMP
+
+	# Upload to S3
+	s3cmd put mongodb-$i-$TIMESTAMP.tar s3://$S3_BUCKET_NAME/$S3_BUCKET_PATH/mongodb-$i-$TIMESTAMP.tar
+
+	#Unlock database writes
+	mongo admin --eval "printjson(db.fsyncUnlock())"
+
+	#Delete local files
+	rm -rf mongodb-*
+
+done
+
