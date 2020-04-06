a407f350bc18bf366e73585e7e3d256d77b2c3be
Abdulmalik Yusuf <themalikyusuf@gmail.com>
2020-04-06 12:33:14 +0100

    feat: add vars

---
+++ windows-ami-automation/variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ADMIN_PASSWORD=""
+AWS_ACCESS_KEY=""
+AWS_SECRET_KEY=""
+
+sed -i "" -e "s/ADMIN_PASSWORD/$ADMIN_PASSWORD/g" provisioners/userdata.ps1 template.json
+
+sed -i "" -e "s/AWS_ACCESS_KEY/$AWS_ACCESS_KEY/g" template.json
+sed -i "" -e "s/AWS_SECRET_KEY/$AWS_SECRET_KEY/g" template.json
